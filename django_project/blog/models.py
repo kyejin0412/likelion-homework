4e17a44eee57ecaf35a4370e8f21b98d96108505
kyejin0412 <kyejin0412@kookmin.ac.kr>
2021-05-10 22:22:18 +0900

    models.py 추가

--- django_project/blog/models.py
+++ django_project/blog/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Blog(models.Model):
+    title = models.CharField(max_length=20)
+    body = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    TF = models.BooleanField()
+    email = models.EmailField()
+    URL = models.URLField()
+    like = models.PositiveIntegerField()
+
+    def __str__(self):
+        return self.title
+
